91c1147a2e4d7dd6cf53c8ba6826daee2ebb9661
Christopher M. Palce <cmpalce4@gmail.com>
2018-05-05 12:30:40 +0800

    update home page

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>techmeetupsph api</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,15 +79,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Techmeetups PH REST API
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://gitlab.com/techmeetupsph/techmeetupsph/blob/master/backend/readme.md">Documentation</a>
+                    <a href="https://techmeetups.ph.com">techmeetupsph</a>
+                    <a href="https://gitlab.com/techmeetupsph/techmeetupsph">GitLab</a>
                 </div>
             </div>
         </div>
